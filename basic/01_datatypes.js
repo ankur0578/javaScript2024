b33018fa4bb7b3c98aec700a7b0011b79d4a1bce
Ankur Chauhan <81966472+ankur0578@users.noreply.github.com>
2024-01-16 07:56:39 +0000

    datatypes

---
+++ basic/01_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js as new  version
+
+//alert(3+3) we using node js not browser
+
+let name = "ankur"; // string
+let age  =  33; // integer
+let isTrue = true; //boolean
+
+// null => standalone value (representation of empty value)
+//undefined  => 
+//symbol => unique
+
+
+//object 
+
+console.log(typeof age) 
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)  //object
---
+++ basic/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 123;
+let account_email = "test@google.com";
+var account_password  = "asjdfass";
+city = "jaipur";
+let state
+
+//accountId= 12; not allowed const cannot be changed
+
+account_email = "test1@googl.com";
+account_password="asdjfaskf"
+city="delhi"
+
+/*
+preferred not to use var because of scope issue
+*/
+
+console.log(accountId);
+console.table([accountId,account_email, account_password,city, state])
